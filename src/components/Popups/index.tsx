abf8e99d7bdff3eb3d5016cbc0033273663770cb
wangheng <1339669469@qq.com>
2024-08-18 20:08:02 +0800

    fix：修改导入

--- src/components/Popups/index.tsx
+++ src/components/Popups/index.tsx
@@ -1,73 +1,75 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import { useActivePopups } from '../../state/application/hooks'
-import { AutoColumn } from '../Column'
-import PopupItem from './PopupItem'
+// import styled from 'styled-components'
+import { useActivePopups, useRemovePopup } from '../../state/application/hooks'
+// import { AutoColumn } from '../Column'
+// import PopupItem from './PopupItem'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../state'
 import ReactGA from 'react-ga'
 import { acceptListUpdate } from '../../state/lists/actions'
 
-const MobilePopupWrapper = styled.div<{ height: string | number }>`
-  position: relative;
-  max-width: 100%;
-  height: ${({ height }) => height};
-  margin: ${({ height }) => (height ? '0 auto;' : 0)};
-  margin-bottom: ${({ height }) => (height ? '20px' : 0)}};
+// const MobilePopupWrapper = styled.div<{ height: string | number }>`
+//   position: relative;
+//   max-width: 100%;
+//   height: ${({ height }) => height};
+//   margin: ${({ height }) => (height ? '0 auto;' : 0)};
+//   margin-bottom: ${({ height }) => (height ? '20px' : 0)}};
 
-  display: none;
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    display: block;
-  `};
-`
+//   display: none;
+//   ${({ theme }) => theme.mediaWidth.upToSmall`
+//     display: block;
+//   `};
+// `
 
-const MobilePopupInner = styled.div`
-  height: 99%;
-  overflow-x: auto;
-  overflow-y: hidden;
-  display: flex;
-  flex-direction: row;
-  -webkit-overflow-scrolling: touch;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
+// const MobilePopupInner = styled.div`
+//   height: 99%;
+//   overflow-x: auto;
+//   overflow-y: hidden;
+//   display: flex;
+//   flex-direction: row;
+//   -webkit-overflow-scrolling: touch;
+//   ::-webkit-scrollbar {
+//     display: none;
+//   }
+// `
 
-const FixedPopupColumn = styled(AutoColumn)`
-  position: fixed;
-  top: 64px;
-  right: 1rem;
-  max-width: 355px !important;
-  width: 100%;
-  z-index: 2;
+// const FixedPopupColumn = styled(AutoColumn)`
+//   position: fixed;
+//   top: 64px;
+//   right: 1rem;
+//   max-width: 355px !important;
+//   width: 100%;
+//   z-index: 2;
 
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    display: none;
-  `};
-`
+//   ${({ theme }) => theme.mediaWidth.upToSmall`
+//     display: none;
+//   `};
+// `
 
 export default function Popups() {
   // get all popups
   const activePopups = useActivePopups()
-  console.log('activePopups ----- ', activePopups)
-  // const removePopup = useRemovePopup()
+  console.log('activePopups -----========== ', activePopups)
+  const removePopup = useRemovePopup()
   const dispatch = useDispatch<AppDispatch>()
   useEffect(() => {
     if (activePopups[0]) {
       const data: any = activePopups[0].content
-      ReactGA.event({
-        category: 'Lists',
-        action: 'Update List from Popup',
-        label: data.listUpdate?.listUrl
-      })
-      dispatch(acceptListUpdate(data.listUpdate?.listUrl))
-      // removePopup(activePopups[0].key)
+      if ('listUpdate' in data) {
+        ReactGA.event({
+          category: 'Lists',
+          action: 'Update List from Popup',
+          label: data.listUpdate?.listUrl
+        })
+        dispatch(acceptListUpdate(data.listUpdate?.listUrl))
+      }
+      removePopup(activePopups[0].key)
     }
-  }, [])
+  }, [activePopups])
 
   return (
     <>
-      <FixedPopupColumn gap="20px">
+      {/* <FixedPopupColumn gap="20px">
         {activePopups.map(item => (
           <PopupItem key={item.key} content={item.content} popKey={item.key} removeAfterMs={item.removeAfterMs} />
         ))}
@@ -81,7 +83,7 @@ export default function Popups() {
               <PopupItem key={item.key} content={item.content} popKey={item.key} removeAfterMs={item.removeAfterMs} />
             ))}
         </MobilePopupInner>
-      </MobilePopupWrapper>
+      </MobilePopupWrapper> */}
     </>
   )
 }
